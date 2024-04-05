3af38a5895492138b82312f0e0559b415c2ec725
SCR-athif <80390996+SCR-athif@users.noreply.github.com>
2024-04-05 17:21:01 +0530

    Update webt.py
    Coloring

--- webt.py
+++ webt.py
@@ -8,6 +8,7 @@ import ssl
 import sys
 import socket
 from urllib.parse import urlparse
+from colorama import Fore, Back, Style
 
 # Suppress InsecureRequestWarning
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -29,7 +30,7 @@ def check_ssl_version_and_ciphers(url):
         "--mozilla_config=intermediate",
         f"{host}:{port}"
     ]
-    print("\n"+"\033[1m" + "Checking SSL Ciphers" + "\033[0m")
+    print(Fore.BLUE+"\n"+"\033[1m" + "Checking SSL Ciphers" + "\033[0m")
     try:
         # Run sslyze as a subprocess
         output= subprocess.check_output(command, stderr=subprocess.STDOUT, text=True)
@@ -58,39 +59,40 @@ def check_security_headers(url):
         # Check if each security header is present in the response headers
         present_headers = [header for header in security_headers if header in response_headers]
         missing_headers = [header for header in security_headers if header not in response_headers]
-        print("\n"+"\033[1m" + "Checking Security Headers" + "\033[0m")
+        print(Fore.BLUE+"\n"+"\033[1m" + "Checking Security Headers" + "\033[0m")
         if present_headers:
-            print("\033[1m"+"\nThese headers are present:"+"\033[0m")
+            print(Fore.YELLOW+"\033[1m"+"\nThese headers are present:"+"\033[0m"+"\n")
             for header in present_headers:
-                print(header)
+                print(Fore.GREEN+header)
         else:
-            print("\033[1m"+"\nNo security headers are present."+"\033[0m")
+            print(Fore.RED+"\033[1m"+"\nNo security headers are present."+"\033[0m"+"\n")
 
         if missing_headers:
-            print("\033[1m"+"\nThese headers are not present:"+"\033[0m")
+            print(Fore.YELLOW+"\033[1m"+"\nThese headers are not present:"+"\033[0m")
             for header in missing_headers:
-                print("Missing Security Header: "+header+" is not implemented")
+                print(Fore.RED+"Missing Security Header: "+header+" is not implemented")
         else:
-            print("\033[1m"+"\nAll security headers are present."+"\033[0m")
+            print(Fore.GREEN+"\033[1m"+"\nAll security headers are present."+"\033[0m")
 
 
         # Check if "Server" header is present
-        print("\n"+"\033[1m" + "Checking Server Header disclosure" + "\033[0m")
+        print(Fore.BLUE+"\n"+"\033[1m" + "Checking Server Header disclosure" + "\033[0m")
         if "Server" in response_headers:
-            print("\nServer header is disclosed:", response_headers["Server"])
+            print(Fore.RED+"\nServer header is disclosed:", response_headers["Server"])
         else:
-            print("\nServer header is not present.")
+            print(Fore.GREEN+"\nServer header is not present.")
         # Check if "X-Powered-BY" header is present
-        print("\n"+"\033[1m" + "Checking X-Powered-BY Header disclosure" + "\033[0m")
+        print(Fore.BLUE+"\n"+"\033[1m" + "Checking X-Powered-BY Header disclosure" + "\033[0m")
         if "x-powered-by" in response_headers:
-            print("\nX-Powered-BY header is disclosed:", response_headers["x-powered-by"])
+            print(Fore.RED+"\nX-Powered-BY header is disclosed:", response_headers["x-powered-by"])
         else:
-            print("\nX-Powered-BY header is not present.")
+            print(Fore.GREEN+"\nX-Powered-BY header is not present.")
+
     except requests.exceptions.RequestException as e:
         print("Error occurred while fetching headers:", e)
 def check_options_method_allowed(url):
     try:
-        print("\n"+"\033[1m" + "Checking for option methods" + "\033[0m")
+        print(Fore.BLUE+"\n"+"\033[1m" + "Checking for option methods" + "\033[0m")
         # Run curl command to send OPTIONS request
         curl_command = f'curl -X OPTIONS --head {url} -k'
 
@@ -100,23 +102,23 @@ def check_options_method_allowed(url):
         decoded_output = output.decode('utf-8')
         # Check if the response includes "Allow" header
         if 'Access-Control-Allow-Methods:' in decoded_output:
-            print("OPTIONS method is Enabled.")
+            print(Fore.RED+"\nOPTIONS method is Enabled.")
             # Extract allowed methods from the response
             allowed_methods = [method.strip() for method in decoded_output.split('Access-Control-Allow-Methods:')[1].split('\n')[0].split(',')]
             print("Allowed methods:", allowed_methods)
         elif 'Allow:' in decoded_output:
-            print("OPTIONS method is Enabled.")
+            print(Fore.RED+"\nOPTIONS method is Enabled.")
             # Extract allowed methods from the response
             allowed_methods = [method.strip() for method in decoded_output.split('Allow:')[1].split('\n')[0].split(',')]
             print("Allowed methods:", allowed_methods)
         else:
-            print("OPTIONS method is not Enabled.")
+            print(Fore.GREEN+"\nOPTIONS method is not Enabled.")
 
     except subprocess.CalledProcessError as e:
         print("Error occurred while running curl command:", e.output.decode('utf-8'))
 def check__cookie_without_secure_flag(url):
     try:
-        print("\n"+"\033[1m" + "Checking Cookie is set without secure flag or not" + "\033[0m")
+        print(Fore.BLUE+"\n"+"\033[1m" + "Checking Cookie is set without secure flag or not" + "\033[0m")
 
         # Run curl command to check cookie request
         curl_command = f'curl --head {url} -k'
@@ -127,25 +129,56 @@ def check__cookie_without_secure_flag(url):
         decoded_output = output.decode('utf-8')
         if 'Set-Cookie' in decoded_output:
             if 'Secure'in decoded_output:
-                print("\nSecure flag are set: Safe")
+                print(Fore.GREEN+"\nSecure flag are set: Safe")
             elif 'secure'in decoded_output:
-                print("\nSecure flag are set: Safe")
+                print(Fore.GREEN+"\nSecure flag are set: Safe")
             else:
-                print("\nCookies without secure flagset : Vulnerable")
+                print(Fore.RED+"\nCookies without secure flagset : Vulnerable")
         elif 'set-cookie' in decoded_output:
             if 'Secure'in decoded_output:
-                print("\nSecure flag are set: Safe")
+                print(Fore.GREEN+"\nSecure flag are set: Safe")
             elif 'secure'in decoded_output:
-                print("\nSecure flag are set: Safe")
+                print(Fore.GREEN+"\nSecure flag are set: Safe")
 
             else:
-                print("\nCookies without secure flagset : Vulnerable")
+                print(Fore.RED+"\nCookies without secure flagset : Vulnerable")
 
         else:
-            print("\nNo cookies are set.")
+            print(Fore.YELLOW+"\nNo cookies are set.")
 
     except subprocess.CalledProcessError as e:
         print("Error occurred while running curl command:", e.output.decode('utf-8'))
+
+def poc_curl(url):
+    print(Fore.BLUE+"\n"+"\033[1m" + "HEADER POC" + "\033[0m"+"\n")
+ 
+    # Command to execute
+    command = f"curl {url} -I -k"
+
+    # Execute the command
+    exit_code = os.system(command)
+
+    # Check if the command executed successfully
+    if exit_code == 0:
+       print(Fore.GREEN+"Command executed successfully.")
+    else:
+       print(Fore.RED+"Error executing command.")
+
+# Function to get user input for URL
+def poc_opt(url):
+    print(Fore.BLUE+"\n"+"\033[1m" + "OPTION POC" + "\033[0m"+"\n")
+ 
+    # Command to execute
+    command = f"curl -X OPTION {url} -I -k"
+
+    # Execute the command
+    exit_code = os.system(command)
+
+    # Check if the command executed successfully
+    if exit_code == 0:
+       print(Fore.GREEN+"Command executed successfully.")
+    else:
+       print(Fore.RED+"Error executing command.")
 
 if __name__ == "__main__":
     os.system('clear')
@@ -157,4 +190,6 @@ if __name__ == "__main__":
     check_security_headers(url)
     check_options_method_allowed(url)
     check__cookie_without_secure_flag(url)
+    poc_curl(url)
+    poc_opt(url)
     check_ssl_version_and_ciphers(url)
